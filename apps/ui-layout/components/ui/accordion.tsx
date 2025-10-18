f95afea45ab0a2f19dd3fe1c044d4eff9e42257b
Dipak Dhariyaparmar <152061206+Dipak-Dhariyaparmar@users.noreply.github.com>
2025-10-18 12:13:48 +0530

    Update accordion.tsx

--- apps/ui-layout/components/ui/accordion.tsx
+++ apps/ui-layout/components/ui/accordion.tsx
@@ -1,4 +1,4 @@
-//This is an accordion screen related component
+//This is an accordion screen related components
 
 "use client"
 import React, { ReactNode, ReactElement, isValidElement } from 'react';
